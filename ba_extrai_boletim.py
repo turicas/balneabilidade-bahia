f0158a49d2eef0267f22ea82f0469620e5c9db56
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-11-23 15:09:06 -0200

    Aplica black (PEP8)

--- ba_extrai_boletim.py
+++ ba_extrai_boletim.py
@@ -9,43 +9,46 @@ from rows.utils import open_compressed
 from ba_parse_pdf import extract_table
 
 
-DOWNLOAD_PATH = Path(__file__).parent / 'data/download'
-OUTPUT_PATH = Path(__file__).parent / 'data/output'
+DOWNLOAD_PATH = Path(__file__).parent / "data/download"
+OUTPUT_PATH = Path(__file__).parent / "data/output"
 if not DOWNLOAD_PATH.exists():
     DOWNLOAD_PATH.mkdir()
 
 
 class ExtraiBoletins(scrapy.Spider):
 
-    name = 'balneabilidade-ba-extrai-boletim'
+    name = "balneabilidade-ba-extrai-boletim"
 
     def start_requests(self):
 
-        filename = glob.glob(str(OUTPUT_PATH / 'ba-boletim.csv*'))[0]
-        fobj = open_compressed(filename, encoding='utf8')
+        filename = glob.glob(str(OUTPUT_PATH / "ba-boletim.csv*"))[0]
+        fobj = open_compressed(filename, encoding="utf8")
         for row in csv.DictReader(fobj):
             filename = DOWNLOAD_PATH / f'BA/{row["id_campanha"]}.pdf'
             if not filename.parent.exists():
                 filename.parent.mkdir()
-            row['filename'] = filename
+            row["filename"] = filename
             if filename.exists():
-                url = 'file://' + str(filename.absolute())
+                url = "file://" + str(filename.absolute())
             else:
-                url = row['url']
+                url = row["url"]
 
             yield scrapy.Request(url=url, meta=row)
 
     def parse(self, response):
         meta = response.request.meta.copy()
-        filename = meta['filename']
-        meta['costa_menu'] = meta['costa']
-        del_keys = [key for key in meta.keys()
-                    if key.startswith('download_')
-                    or key in ('url', 'filename', 'depth', 'costa')]
+        filename = meta["filename"]
+        meta["costa_menu"] = meta["costa"]
+        del_keys = [
+            key
+            for key in meta.keys()
+            if key.startswith("download_")
+            or key in ("url", "filename", "depth", "costa")
+        ]
         for key in del_keys:
             del meta[key]
         if not filename.exists():
-            with open(filename, mode='wb') as fobj:
+            with open(filename, mode="wb") as fobj:
                 fobj.write(response.body)
 
         for row in extract_table(io.BytesIO(response.body)):
--- ba_lista_boletim.py
+++ ba_lista_boletim.py
@@ -1,31 +1,36 @@
 from scrapy import Request, Spider
 
 
-URL_BOLETIM = 'http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/geraBoletim?idcampanha={id_campanha}'
+URL_BOLETIM = "http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/geraBoletim?idcampanha={id_campanha}"
 
 
 class ListaBoletinsSpider(Spider):
 
-    name = 'balneabilidade-ba-lista-boletim'
-    start_urls = ['http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/boletim']
+    name = "balneabilidade-ba-lista-boletim"
+    start_urls = [
+        "http://balneabilidade.inema.ba.gov.br/index.php/relatoriodebalneabilidade/boletim"
+    ]
 
     def parse(self, response):
         for campanha in response.xpath('//select[@id="idcampanha"]/option'):
-            id_campanha = campanha.xpath('./@value').extract()[0]
-            if id_campanha == '-1':  # 'TODAS AS CAMPANHAS'
+            id_campanha = campanha.xpath("./@value").extract()[0]
+            if id_campanha == "-1":  # 'TODAS AS CAMPANHAS'
                 continue
-            partes = (campanha.xpath('./text()').extract()[0]
-                              .strip()
-                              .replace('CAMPANHA EMERG', 'CAMPANHA_EMERG')
-                              .split())
-            partes_data = [int(parte) for parte in partes[-1].split('/')]
-            data = f'{partes_data[2]}-{partes_data[1]:02d}-{partes_data[0]:02d}'
+            partes = (
+                campanha.xpath("./text()")
+                .extract()[0]
+                .strip()
+                .replace("CAMPANHA EMERG", "CAMPANHA_EMERG")
+                .split()
+            )
+            partes_data = [int(parte) for parte in partes[-1].split("/")]
+            data = f"{partes_data[2]}-{partes_data[1]:02d}-{partes_data[0]:02d}"
 
             yield {
-                'costa': partes[2],
-                'data_emissao': data,
-                'id_campanha': id_campanha,
-                'numero_boletim': partes[1],
-                'tipo_campanha': partes[0],
-                'url': URL_BOLETIM.format(id_campanha=id_campanha),
+                "costa": partes[2],
+                "data_emissao": data,
+                "id_campanha": id_campanha,
+                "numero_boletim": partes[1],
+                "tipo_campanha": partes[0],
+                "url": URL_BOLETIM.format(id_campanha=id_campanha),
             }
